51b6d792a5a7a4710546f633a58d11ba3af4d2f9
Kimlyn DeLara <klyndelara@gmail.com>
2023-10-30 17:43:21 -0700

    Testing

--- src/main/java/com/KimlynsPractice/demo/controller/CarsController.java
+++ src/main/java/com/KimlynsPractice/demo/controller/CarsController.java
@@ -2,11 +2,12 @@ package com.KimlynsPractice.demo.controller;
 
 import com.KimlynsPractice.demo.dao.CarsDao;
 import com.KimlynsPractice.demo.model.Cars;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
 
+import java.security.Principal;
 import java.util.List;
 
 @RestController
@@ -21,5 +22,39 @@ public class CarsController {
     }
     @RequestMapping(path = "", method = RequestMethod.GET)
     public List<Cars> getCars() { return carsDao.getCars(); }
+
+    @RequestMapping(path = "/{id}", method = RequestMethod.GET)
+    public Cars getCar(@PathVariable int id) {
+        Cars cars = carsDao.getCarById(id);
+
+        if (cars == null) {
+            throw new ResponseStatusException(HttpStatus.NOT_ACCEPTABLE, "Car Not Found");
+        }else {
+            return cars;
+        }
+    }
+
+    @RequestMapping(path = "", method = RequestMethod.POST)
+    public Cars create(@RequestBody Cars car) {
+
+        int carId = carsDao.insert(car);
+        return carsDao.getCarById(carId);
+    }
+    //            try {
+//            carId = carsDao.insert(car);
+//        } catch(ResponseStatusException e){
+//            e.getReason();
+//        }
+//            return carsDao.getCarById(carId);
+//
+//
+//    }
+    @RequestMapping(path = "/{id}", method = RequestMethod.DELETE)
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void removeCar(@PathVariable int id) {
+        carsDao.delete(id);
+    }
+
+
 
 }
--- src/main/java/com/KimlynsPractice/demo/dao/MembersDao.java
+++ src/main/java/com/KimlynsPractice/demo/dao/MembersDao.java
@@ -1,4 +1,17 @@
 package com.KimlynsPractice.demo.dao;
 
+import com.KimlynsPractice.demo.model.Members;
+
+import java.util.List;
+
 public interface MembersDao {
+
+
+    List<Members> getMembers();
+
+    Members getMemberById(int memberId);
+
+    int insertMember(Members member);
+
+    int deleteMember(int memberId);
 }
